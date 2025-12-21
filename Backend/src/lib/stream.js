bd1d6465f47c8fe9111dd1a860502811a9fc183a
Atul singh <atusingh1618@gmail.com>
2025-12-21 21:29:19 +0530

    added stream auth sync

---
+++ Backend/src/lib/stream.js
@@ -0,0 +1,30 @@
+import {StreamChat} from "stream-chat";
+import { ENV } from "./env.js";
+import { useId } from "react";
+
+const apiKey = ENV.STREAM_API_KEY
+const apiSecret = ENV.STREAM_SECRET_KEY
+
+if(!apiKey || !apiSecret){
+    console.log("STREAM_API_KEY or STREAM_SECRET_KEY is missing")
+}
+
+export const chatClient =StreamChat.getInstance(apiKey,apiSecret)
+
+export const upsertStreamUser = async(userData) =>{
+    try {
+        await chatClient.upsertUser(userData)
+        console.log("stream user deleted successfully : ",userData)
+    } catch (error) {
+        console.error("Error upserting the stream user: ",error)
+    }
+}
+
+export const deleteStreamUser = async(userId) =>{
+    try {
+        await chatClient.deleteUser(userId)
+        console.log("stream user deleted successfully : ",userId)
+    } catch (error) {
+        console.error("Error deleting the stream user: ",error)
+    }
+}
