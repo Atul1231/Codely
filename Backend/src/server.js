ca04e2d2a36524673e553a909de8b1b3d5005a9a
Atul singh <atusingh1618@gmail.com>
2025-12-22 17:15:50 +0530

    server side authentication implementation

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,25 +4,19 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import {serve} from "inngest/express"
 import { inngest ,functions} from "./lib/inngest.js";
-
+import {clerkMiddleware} from '@clerk/express'
+import chatRoutes from "./routes/chatRoutes.js"
 const app = express();
 
 app.use(express.json())
-
-app.get("/",(req,res) => {
-    res.status(200).json({msg:"success from the backend"})
-});
-
-app.get("/books",(req,res) => {
-    res.status(200).json({msg:"success from the backend of books"})
-});
-
 // credential = true means the server allows the browser to include cookies on request
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+app.use(clerkMiddleware())   // this adds auth field to  request object req.auth()
+
 
 app.use("/api/inngest" , serve({client:inngest , functions}))
 // console.log("Signing key exists:", !!process.env.INNGEST_SIGNING_KEY);
-
+app.use("/api/chat",chatRoutes)
 const ServerConnect = async() =>{
     try{
         await connectDB();
