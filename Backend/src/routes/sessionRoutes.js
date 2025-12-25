cb6d77ece9f148f3c122eec517d1e036a4648b0d
Atul singh <atusingh1618@gmail.com>
2025-12-25 16:04:13 +0530

    minor bug fixes

--- Backend/src/routes/sessionRoutes.js
+++ Backend/src/routes/sessionRoutes.js
@@ -9,7 +9,7 @@ router.get("/active",protectRoute,getActiveSessions);
 router.get("/my-recent",protectRoute,getMyRecentSessions);
 router.post("/:id/join",protectRoute,joinSession);
 router.get("/:id",protectRoute,getSessionById);
-router.post(":id/end",protectRoute,endSession);
+router.post("/:id/end",protectRoute,endSession);
 //api/session/7136129
 
 export default router
