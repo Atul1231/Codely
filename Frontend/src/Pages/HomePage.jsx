55fc949ea84b3046bdd3ecd1547c5129d5d251eb
Atul singh <atusingh1618@gmail.com>
2025-12-27 23:20:39 +0530

    minor changes

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -13,7 +13,7 @@ import ThemeToggle from "../components/ThemeToggle";
 
 function HomePage() {
   return (
-    <div className="bg-linear-to-br from-base-100 via-base-200 to-base-300">
+    <div className="bg-gradient-to-br from-base-100 via-base-200 to-base-300">
       {/* NAVBAR */}
       <nav className="bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg">
         <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
