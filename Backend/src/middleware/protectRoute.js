fa4dd24aae64d9427b86715cbe112df9ce767cc0
Atul singh <atusingh1618@gmail.com>
2026-01-01 00:52:51 +0530

    minor changes

--- Backend/src/middleware/protectRoute.js
+++ Backend/src/middleware/protectRoute.js
@@ -2,24 +2,22 @@ import { requireAuth } from "@clerk/express";
 import User from "../models/User.js";
 
 export const protectRoute = [
-  requireAuth(),
+  requireAuth(), // Clerk validates session
   async (req, res, next) => {
     try {
-      if (!req.auth || !req.auth.userId) {
+      if (!req.auth?.userId) {
         return res.status(401).json({ message: "Unauthorized" });
       }
 
       const clerkId = req.auth.userId;
-
       const user = await User.findOne({ clerkId });
-      if (!user) {
-        return res.status(401).json({ message: "User not found" });
-      }
+
+      if (!user) return res.status(401).json({ message: "User not found" });
 
       req.user = user;
       next();
-    } catch (error) {
-      console.error("protectRoute error:", error);
+    } catch (err) {
+      console.error("ProtectRoute error:", err);
       res.status(500).json({ message: "Internal server error" });
     }
   },
