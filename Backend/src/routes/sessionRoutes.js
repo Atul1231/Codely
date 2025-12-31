a8a19c005370acdf852c143a92ccae654dea3b2b
Atul singh <atusingh1618@gmail.com>
2025-12-31 16:04:06 +0530

    testing routes in production

--- Backend/src/routes/sessionRoutes.js
+++ Backend/src/routes/sessionRoutes.js
@@ -4,12 +4,12 @@ import { endSession } from "../controllers/sessionController.js"
 import { createSession , getActiveSessions,getMyRecentSessions,joinSession,getSessionById } from "../controllers/sessionController.js"
 const router = express.Router()
 
-router.post("/",protectRoute,createSession);
-router.get("/active",protectRoute,getActiveSessions);
-router.get("/my-recent",protectRoute,getMyRecentSessions);
-router.post("/:id/join",protectRoute,joinSession);
-router.get("/:id",protectRoute,getSessionById);
-router.post("/:id/end",protectRoute,endSession);
+router.post("/",createSession);
+router.get("/active",getActiveSessions);
+router.get("/my-recent",getMyRecentSessions);
+router.post("/:id/join",joinSession);
+router.get("/:id",getSessionById);
+router.post("/:id/end",endSession);
 //api/session/7136129
 
 export default router
