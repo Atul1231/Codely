ec781e8158006ee87a2017e6a20f8a9fc599e1db
Atul singh <atusingh1618@gmail.com>
2025-12-31 14:00:13 +0530

    minor change

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -3,5 +3,6 @@ const axiosInstance = axios.create({
     baseURL:import.meta.env.VITE_API_BASE_URL,
     withCredentials:true,
 });
+console.log("AXIOS BASE URL:", axiosInstance.defaults.baseURL);
 
 export default axiosInstance;
