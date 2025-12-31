b7a7aa53b9383650143ec334322d9a605acd31ab
Atul singh <atusingh1618@gmail.com>
2025-12-31 23:25:51 +0530

    testing routes in production

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,13 @@
 import axios from "axios"
+const API_URL = import.meta.env.VITE_API_URL;
+if (!API_URL) {
+  throw new Error("VITE_API_URL is not defined");
+}
 const axiosInstance = axios.create({
-    baseURL:import.meta.env.VITE_API_URL,
+    baseURL:API_URL,
     withCredentials:true,
 });
 // console.log("AXIOS BASE URL:", import.meta.env.VITE_API_BASE_URL);
+console.log(axiosInstance.defaults.baseURL);
 
 export default axiosInstance;   
