ddc454c23caf600c2317d34629beafb56734a146
Atul singh <atusingh1618@gmail.com>
2026-01-01 00:17:32 +0530

    minor changes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
+app.use("/api",clerkMiddleware()); // this adds auth field to request object: req.auth()
 app.use((req, res, next) => {
   console.log("AUTH:", req.auth);
   next();
